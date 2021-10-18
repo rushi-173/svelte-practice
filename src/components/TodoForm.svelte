116e2b6f2624bb870873135a0ebd5d58a344cb1c
Rushikesh Chaudhari <rushichaudhari1237@gmail.com>
2021-10-18 19:45:26 +0530

    created components, passed props, and rendered content

---
+++ src/components/TodoForm.svelte
@@ -0,0 +1,10 @@
+<script>
+	export let todos = [];
+</script>
+
+<main>
+	<h1>TodoForm</h1>
+</main>
+
+<style>
+</style>
---
+++ src/components/TodoList.svelte
@@ -0,0 +1,11 @@
+<script>
+	export let todos = [];
+</script>
+
+<main>
+	<h2>TodoList</h2>
+	<p>{todos[0].task}</p>
+</main>
+
+<style>
+</style>
